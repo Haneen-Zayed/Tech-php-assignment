9b2d4e92a90bee75a53a0765041dd21f435cc37a
Haneen_Zayed <76243758+Haneen6@users.noreply.github.com>
2022-11-12 20:29:21 +0200

    assignment PHP pages- HTU courses 12.11.2022

---
+++ contact_us.php
@@ -0,0 +1,18 @@
+<?php include './header.php';?>
+
+<div class="container m-auto p-5 d-flex justify-content-center align-items-center row col-12">
+    <h1 class="text-bg-warning text-center p-2">Contact us</h1>
+    <p class="pt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Quidem beatae sed mollitia dolor nemo. Quo corporis nesciunt nobis aut iusto ut debitis
+        ex minus reiciendis ducimus est, autem velit nemo! Lorem ipsum dolor
+         sit amet consectetur adipisicing elit. Voluptates impedit veniam omnis
+          similique perspiciatis odit? Maiores laudantium doloribus, dolore,
+           facere ipsa distinctio, voluptas impedit unde
+         commodi iste eaque nobis eveniet? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Temporibus unde dolorum tempore? Illum, aliquam hic in eaque,
+          laudantium beatae, porro cumque ducimus doloremque ullam quasi.
+           Voluptates aliquam excepturi impedit enim!</p>
+</div>
+
+
+<?php include './footer.php'; ?>
